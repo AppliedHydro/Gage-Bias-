c47d06bf420e395f385314c0983a225eb015a661
stevenschmitz <stevenschmitz@u.boisestate.edu>
2023-11-15 16:16:58 -0700

    Newly updated and added scripts. Woopedy doo! Progress.

--- editor/Catalog_Subsetting.py
+++ editor/Catalog_Subsetting.py
@@ -8,6 +8,7 @@ cat_dir = 'C:/Users/stevenschmitz/Desktop/PlacementBias/Streamflow_Catalog.csv'
 err_msg = 'Dataframe not loaded correctly - check output'
 df = pd.read_csv(cat_dir)
 
+# Defining functions of code subsetting
 def get_USGS(df) -> pd.DataFrame:
     """
     Takes the Streamflow Catalog and filters out all rows that are not
@@ -50,6 +51,40 @@ def get_disc(df) -> pd.DataFrame:
     print('df_disc has {} gages'.format(len(df_disc)))
     df_disc.to_csv(wrk_dir + 'Discrete_cat.csv',index=False)
     return df_disc
+
+# Initialization point of the code
+
+# Excel spreadsheet with callable columns
+column_file = #####
+print('Refer to {} for a list of columns names. Use column names as input to '
+      'uniques() in string format to find all available unique values in'
+      'that column. These unique values can be used as inputs to create new'
+      '.xlsx with subset gages.'.format(column_file))
+
+def uniques(file_path, sheet_name = 'Sheet1', column_name):
+    '''
+    Reads unique columns values to be used for subsetting the output streamflow catalog.
+    :param file_path: file path of streamflow catalog
+    :param sheet_name: in default streamflow catalog, Sheet1 is the primary catalog sheet.
+    :param column_name: column to find unique values from.
+    :return: list of unique values
+    '''
+    try:
+        df = pd.read_excel(file_path, sheet_name=sheet_name)
+        unique_values = df[column_name].unique()
+        return unique_values
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+def master_subset(file_path, column_name, target_value,output_path):
+    try:
+        df = pd.read_excel(file_path)
+        filtered_df = df[df[column_name] == target_value]
+        filtered_df.to_csv(output_path, index=False)
+        print(f"Rows with '{target_value}' saved to {output_file_path}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == '__main__':
     #Filter catalog for USGS gages
