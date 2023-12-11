b16b6be568d8191188bb8cb402622ec1633e68db
Steven Schmitz <120681809+sjschmitz@users.noreply.github.com>
2023-12-11 13:31:42 -0700

    Update join_gauge_GRADES.py

--- primary/join_gauge_GRADES.py
+++ primary/join_gauge_GRADES.py
@@ -1,3 +1,10 @@
+# join_gauge_GRADES.py
+#
+# Author: Steven Schmitz
+# date: 11.12.2023
+# Modified for Streamflow Catalog Gage Placement Analysis Project from original file below
+# Source: https://github.com/dry-rivers-rcn/G4
+#
 # Assessing placement bias of the global river gauge network
 # Nature Sustainability
 # Authors: Corey A. Krabbenhoft, George H. Allen, Peirong Lin, Sarah E. Godsey, Daniel C. Allen, Ryan M. Burrows, Amanda G. DelVecchia, Ken M. Fritz, Margaret Shanafield
@@ -14,53 +21,47 @@ import geopandas as gpd
 import pandas as pd
 from shapely.geometry import Point
 
+catalog = 'H:/Final_data.csv'
 wrk_dir = 'H:/GRADES/'
+grades_seg = 'H:/GRADES/GRADES_eco.shp'
+
 
 def find_nearest_river(dfpp, dfll, buffersize):
-    '''
-    This function finds the nearest river reach ID for each gauge
-    input: dfpp: point shapefile of the gauges; dfll: line shapefile of GRADES
-    '''
-    # create buffer
-    print('   create buffer... wait ...')
     poly = dfpp.buffer(buffersize)
     polygpd = gpd.GeoDataFrame(dfpp[['Gage_No', 'long', 'lat']], geometry=poly)
     polygpd.crs = 'EPSG:4326'
-    # spatial join
-    print('   spatial join with flowlines.. wait ...')
-    join = gpd.sjoin(polygpd, dfll, how='inner', op='intersects')
-    merge = join.merge(dfll, on='COMID', how='left')
-    print('   calculating distance.. wait ...')
-    merge['distance'] = [Point(merge['long'][i], merge['lat'][i]).distance(merge['geometry_y'][i]) for i in
-                         range(0, len(merge))]
-    join11 = merge.groupby(['Gage_No']).agg({'distance': 'min'}).reset_index()  # min dist: width and MERIT
-    merge11 = join11.merge(merge, on=['Gage_No', 'distance'], how='left')
-    final = merge11[['Gage_No', 'COMID', 'distance', 'long', 'lat']]
+
+    # Spatial join
+    join = gpd.sjoin(polygpd, dfll, how='left', op='intersects')
+
+    # Create a 'geometry_point' column for points
+    join['geometry_point'] = [Point(lon, lat) for lon, lat in zip(join['long'], join['lat'])]
+
+    # Calculate distance
+    join['distance'] = join.apply(lambda row: row['geometry_point'].distance(row['geometry']) if row['geometry'] else None, axis=1)
+
+    # Find the nearest point for each COMID
+    join11 = join.groupby(['COMID']).agg({'distance': 'min', 'Gage_No': 'first', 'lat': 'first', 'long': 'first'}).reset_index()
+
+    # Merge with dfll to get additional variables
+    final = join11.merge(dfll, on='COMID', how='right')[['Gage_No', 'COMID', 'distance', 'lat', 'long', 'ecoregion']]
 
     return final
 
-
 if __name__ == '__main__':
-    # Choose specific subset of gages from destination folder
-    catalog = 'H:/Catalog_Subsets/Discrete_cat.csv'
     df = pd.read_csv(catalog)[['Gage_No', 'lat', 'long']]
     points = [Point(df.long[j], df.lat[j]) for j in range(len(df))]
-
-    # create GeoDataFrame
     dfpp = gpd.GeoDataFrame(df, geometry=points)
 
-    # read GRADES river segments and perform spatial join
-    buffersize = 0.05  # ~5km
+    buffersize = 0.035
     allpoints = []
 
-    # GRADES river segment downloadable from http://hydrology.princeton.edu/data/mpan/MERIT_Basins/MERIT_Hydro_v07_Basins_v01/pfaf_level_01/
-    grades_seg = 'H:/GRADES/MERIT_Basins_v0.7_PNW/pfaf_07_riv_3sMERIT_PNW.shp'
     print('... intersecting with Grades')
     dfll = gpd.read_file(grades_seg)
+
     allpoints.append(find_nearest_river(dfpp, dfll, buffersize))
     allpoints = pd.concat(allpoints)
 
-    # save to file
-    end_dir = wrk_dir + 'GRADES_gage_discrete.csv'
+    end_dir = wrk_dir + 'GRADES_test.csv'
     print('... writing to %s ...' % end_dir)
     allpoints.to_csv(end_dir, index=False)
