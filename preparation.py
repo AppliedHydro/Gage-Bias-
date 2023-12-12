c8d136728b8be2d135bcc6ef31fe0cfab4d086cc
Steven Schmitz <120681809+sjschmitz@users.noreply.github.com>
2023-12-12 10:58:05 -0700

    Update preparation.py

--- preparation.py
+++ preparation.py
@@ -110,3 +110,32 @@ if __name__ == '__main__':
     end_dir = wrk_dir + 'GRADES_test.csv'
     print('... writing to %s ...' % end_dir)
     allpoints.to_csv(end_dir, index=False)
+
+#---------------------------------------------------------------------------#
+# spatially joining GRADES/gage dataset with HydroATLAS
+#---------------------------------------------------------------------------#
+
+def Data_final(attr,gages):
+    df_A, df_B = pd.read_csv(attr),pd.read_csv(gages)
+    assert not df_A.empty,"{} is empty. Reload.".format(attr)
+    assert not df_B.empty,"{} is empty. Reload.".format(gages)
+
+    # merge the DataFrames based on COMID
+    merged_df = pd.merge(df_A, df_B[['COMID', 'Gage_No', 'lat', 'long','ecoregion']], on='COMID', how='left')
+    # save the merged DataFrame to a new CSV file
+    merged_df.to_csv(wrk_dir + 'Final_data.csv', index=False)
+    file_path = wrk_dir + 'Final_data.csv'
+    # read the .csv file and set 'COMID' as the index
+    df = pd.read_csv(file_path)
+    df.set_index('COMID', inplace=True)
+
+    # check for duplicate rows based on 'COMID' index
+    duplicate_rows = df[df.index.duplicated(keep='first')]
+    # remove duplicate rows, keeping only the first instance
+    df = df[~df.index.duplicated(keep='first')]
+    return df
+
+if __name__ == '__main__':
+    df = Data_final(attr,gages)
+    df.reset_index().to_csv(final_path, index=False)
+    print('saved to {}'.format(wrk_dir))
