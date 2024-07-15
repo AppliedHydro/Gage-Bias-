7cd19995a7529cd35d9757756a8e402dc6c6a32f
Steven Schmitz <120681809+sjschmitz@users.noreply.github.com>
2024-07-15 10:56:43 -0600

    Update library.r

--- main/library.r
+++ main/library.r
@@ -1,3 +1,6 @@
+options(warn = -1) # suppresses console warnings
+
+library(here)
 library(tidyverse)
 library(sf)
 library(ggplot2)
@@ -11,11 +14,11 @@ library(leaflet)
 library(rmarkdown)
 library(htmlwidgets)
 
-data <- read.csv('H:/Final_data.csv', header=TRUE)
+data <- read.csv(here('main','Final_data.csv'), header=TRUE)
 
-VARnames <- read.csv('C:/Users/stevenschmitz/Desktop/PlacementBias/Git/G4-master/in/VARnames.csv', header=FALSE)
-ECOnames <- read.csv('C:/Users/stevenschmitz/Desktop/PlacementBias/Git/G4-master/in/ECOnames.csv', header=FALSE)
-FHTnames <- read.csv('C:/Users/stevenschmitz/Desktop/PlacementBias/Git/G4-master/in/FHTnames.csv', header=FALSE)
-VARnames1 <- read.csv('C:/Users/stevenschmitz/Desktop/PlacementBias/Git/G4-master/in/VARnames_noeco.csv', header=FALSE)
+VARnames <- read.csv(here('secondary','VARnames.csv'), header=FALSE)
+ECOnames <- read.csv(here('secondary','ECOnames.csv'), header=FALSE)
+FHTnames <- read.csv(here('secondary','FHTnames.csv'), header=FALSE)
+VARnames1 <- read.csv(here('secondary','VARnames_noeco.csv'), header=FALSE)
 
-shapefile_path <- "H:/gage_bias_master/GRADES_PNW/WatershedHUC8.shp"
+shapefile_path <- here('secondary','WatershedHUC8.shp')
